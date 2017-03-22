eca7f618479350d18bb120d13c2617290d3b7c54
sardylan <sardylan@gmail.com>
2017-03-22 10:28:12 +0100

    Added license header in every source file

--- src/qcw.hpp
+++ src/qcw.hpp
@@ -1,3 +1,24 @@
+/*
+ * qCw
+ * Copyright (C) 2017  Luca Cireddu
+ * sardylan@gmail.com
+ * http://www.lucacireddu.it
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef __QCW_QCW_H
 #define __QCW_QCW_H
 
@@ -8,8 +29,8 @@
 
 #include <config/config.hpp>
 
-#define APPLICATION_NAME "QCw"
-#define APPLICATION_VERSION "0.0.1"
+#define APPLICATION_NAME "qCw"
+#define APPLICATION_VERSION "0.99.0"
 
 #define ORGANIZATION_NAME "The HellNet.org"
 #define ORGANIZATION_DOMAIN "ham.thehellnet.org"
